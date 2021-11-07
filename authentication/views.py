e74e0f4c75a070db1c2c180ab24a60cf88477a28
darkor <romko.kr@gmail.com>
2021-11-07 17:38:22 +0200

    add urls for login, logout, dashboard

--- authentication/views.py
+++ authentication/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.contrib.auth import views
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import TemplateView
 
-# Create your views here.
+
+class CustomLoginView(views.LoginView):
+    template_name = 'registration/login.html'  # Alex you need to update this template
+
+
+class CustomLogoutView(views.LogoutView):
+    template_name = 'registration/logout.html'
+
+
+class DashboardView(LoginRequiredMixin, TemplateView):
+    template_name = 'dashboard.html'
