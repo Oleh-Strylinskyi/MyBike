33876ba163b9a3507d11fbee2d92306ea9b9af3d
darkor <romko.kr@gmail.com>
2021-11-08 23:08:17 +0200

    update model

--- products/models.py
+++ products/models.py
@@ -5,7 +5,7 @@ class Product(models.Model):
     image = models.ImageField()
     name = models.CharField(max_length=20, unique=True)
     description = models.TextField(blank=True, null=True)
-    country_producer = models.ForeignKey('Country', on_delete=models.SET_NULL)
+    country_producer = models.ForeignKey('Country', null=True, on_delete=models.SET_NULL)
     weight = models.DecimalField(max_digits=7, decimal_places=3)
     price = models.DecimalField(max_digits=7, decimal_places=2)
 
