2cd3566799ea359241a56830c0c1cedd9ac75238
darkor <romko.kr@gmail.com>
2021-11-08 23:32:37 +0200

    configure admin panel to add Product and Country

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
 
-# Register your models here.
+from products.models import Product, Country
+
+
+@admin.register(Product)
+class AdminProduct(admin.ModelAdmin):
+    list_display = (
+        'id', 'name', 'country_producer', 'weight', 'price',
+    )
+    list_filter = ('name', 'country_producer', 'weight', 'price',)
+
+
+@admin.register(Country)
+class AdminCountry(admin.ModelAdmin):
+    list_display = (
+        'id', 'name',
+    )
+    list_filter = ('name',)
