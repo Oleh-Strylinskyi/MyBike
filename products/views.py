0a2fff419c38f964a6828bac36b9e66743115da6
darkor <romko.kr@gmail.com>
2021-11-09 00:07:06 +0200

    add products urls, create list and detail views

--- products/views.py
+++ products/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.shortcuts import get_object_or_404
+from django.urls import reverse_lazy
+from django.views.generic import DetailView
+from django.views.generic.list import BaseListView
 
-# Create your views here.
+from products.models import Product
+
+
+class ProductDetailView(LoginRequiredMixin, DetailView):
+    """Get specific product"""
+    model = Product
+    template_name = 'products/product_detail.html'
+    context_object_name = 'product'
+    success_url = reverse_lazy('products:product-detail')
+
+    def get_object(self, queryset=None):
+        return get_object_or_404(self.model, pk=self.kwargs['pk'])
+
+
+class ProductListView(LoginRequiredMixin, BaseListView):
+    """Get list of products"""
+    model = Product
+    # list_filter_class = 'put here some Filter class you created in filter.py'
+    template_name = 'products/product_list.html'
+    success_url = reverse_lazy('products:product-list')
+
+    def get_queryset(self):
+        return self.model.objects.all()
