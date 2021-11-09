45d4d8231f69240d31885c6e1bc754a9dfa862e9
darkor <romko.kr@gmail.com>
2021-11-10 00:35:11 +0200

    add products templates

--- MyBike/urls.py
+++ MyBike/urls.py
@@ -16,10 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from authentication.views import DashboardView
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', DashboardView.as_view(), name='dashboard'),
     path('admin/', admin.site.urls),
     path('auth/', include('authentication.urls')),
     path('products/', include('products.urls')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
