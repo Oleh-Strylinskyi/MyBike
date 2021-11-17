2b69ca95a4072315d6672c2f5b17b4828fb7792c
darkor <romko.kr@gmail.com>
2021-11-17 02:03:08 +0200

    add filtering

--- products/views.py
+++ products/views.py
@@ -1,12 +1,13 @@
 from django.contrib import messages
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.shortcuts import get_object_or_404, redirect, render
+from django.shortcuts import get_object_or_404, redirect
 from django.urls import reverse_lazy
 from django.views import View
 from django.views.generic import DetailView
 from django.views.generic.list import BaseListView, MultipleObjectTemplateResponseMixin
 
-from basket.models import Basket, BasketItem
+from basket.models import Basket
+from products.filters import ProductFilter
 from products.models import Product
 
 
@@ -40,9 +41,17 @@ class AddToBasketView(LoginRequiredMixin, View):
 class ProductListView(LoginRequiredMixin, MultipleObjectTemplateResponseMixin, BaseListView):
     """Get list of products"""
     model = Product
-    # list_filter_class = 'put here some Filter class you created in filter.py'
+    filterset_class = ProductFilter
     template_name = 'products/product_list.html'
     success_url = reverse_lazy('products:product-list')
 
     def get_queryset(self):
-        return self.model.objects.all()
+        queryset = self.model.objects.all()
+        self.filterset = self.filterset_class(self.request.GET, queryset=queryset)
+        return self.filterset.qs
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        # Pass the filterset to the template - it provides the form.
+        context['filterset'] = self.filterset
+        return context
